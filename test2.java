b38928cc0c5f66c2ca39989b9f919d3b24eac7a7
PranshuSharma2811 <pranshusharma2811@gmail.com>
2024-09-17 19:20:39 +0530

    practice

---
+++ test2.java
@@ -0,0 +1,8 @@
+public class test2 {
+    public static void main(String[] args) {
+        int i =5;
+        if(i>4){
+            System.out.println(i);
+        }
+    }
+}
---
+++ test4.java
@@ -0,0 +1,11 @@
+public class test4 {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4};
+        int sum = 0;
+        for(int x : nums){
+         sum+=x;
+
+        }
+        System.out.println("the sum of elements is "+ sum);
+    }
+}
