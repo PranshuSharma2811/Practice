97529ef76271261a0b643684ebde1fc1733587a3
PranshuSharma2811 <pranshusharma2811@gmail.com>
2026-06-24 19:15:01 +0530

    missing number solved using basic maths

---
+++ missing_number_with_maths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int missing_number =0 ;
+        int sum=0;
+        int n =nums.size();
+        for(int i =0;i<n;i++){
+            sum+=nums[i];
+        }
+        
+        int actual_sum = n*(n+1)/2;
+        return actual_sum - sum;
+
+       
+}
+};
