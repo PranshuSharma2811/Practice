4157add4857f594a0cca8fb763c060108b58e203
PranshuSharma2811 <pranshusharma2811@gmail.com>
2026-07-11 23:39:35 +0530

    find duplicate element in an array

---
+++ find-duplicates-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        // code here
+         vector<int> ans;
+        int n=arr.size();
+        for(int i =0;i<n;i++){
+            int index = abs(arr[i]) - 1;
+            if(arr[index]<0){
+                ans.push_back(index+1);
+            }
+            else{
+                arr[index] = -arr[index];
+            }
+        }
+        return ans;
+    }
+};
