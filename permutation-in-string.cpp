9044aaf22d69c6043677197353ddfa6490ee4a50
PranshuSharma2811 <114935404+PranshuSharma2811@users.noreply.github.com>
2026-08-05 01:34:38 +0530

    Add checkInclusion method in Solution class
    Implement checkInclusion method to determine if s1's permutation exists in s2.

---
+++ permutation-in-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size())
+        {
+           return false;
+        }
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+        for(char c :s1){
+            freq1[c - 'a']++;
+        }
+        for (int i = 0; i < s1.size(); i++)
+            {
+                freq2[s2[i] - 'a']++;
+            }
+        if(freq1 == freq2){
+            return true;
+        }
+        int left = 0;
+        for (int right = s1.size(); right < s2.size(); right++){
+            freq2[s2[left] - 'a']--;   // Remove old character
+
+            freq2[s2[right] - 'a']++;  // Add new character
+
+            if (freq1 == freq2)        // Compare
+               return true;
+
+            left++;                    // Slide the window
+        }
+        return false;
+    }
+};
